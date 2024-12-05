ae6c787abd99854d62ad2d46c7b0062e7c422583
Ubaidullah-Hasan <hm2964133@gmail.com>
2024-12-05 22:40:37 +0600

    filterAndMapArray

--- arrayFilteringAndMapping.ts
+++ arrayFilteringAndMapping.ts
@@ -1,4 +1,10 @@
-const people = [
+type TPeople = {
+    name: string;
+    age: number;
+    gender: "male" | "female";
+}
+
+const people: TPeople[] = [
     { name: "John", age: 30, gender: "male" },
     { name: "Jane", age: 25, gender: "female" },
     { name: "Mike", age: 28, gender: "male" },
@@ -6,4 +12,9 @@ const people = [
     { name: "Bob", age: 35, gender: "male" }
 ];
 
-const 
+const nameOfMalePeople = (people: TPeople[]) => {
+    const male = people.filter(p => p.gender !== "female");
+    return male.map(m => m.name);
+}
+
+console.log(nameOfMalePeople(people));
