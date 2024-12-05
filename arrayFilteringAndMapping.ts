40aed3d143c9cb7829055cdeb33f2de6af52fa91
Ubaidullah-Hasan <hm2964133@gmail.com>
2024-12-05 22:30:27 +0600

    filterAndMapOfArray

---
+++ arrayFilteringAndMapping.ts
@@ -0,0 +1,9 @@
+const people = [
+    { name: "John", age: 30, gender: "male" },
+    { name: "Jane", age: 25, gender: "female" },
+    { name: "Mike", age: 28, gender: "male" },
+    { name: "Alice", age: 22, gender: "female" },
+    { name: "Bob", age: 35, gender: "male" }
+];
+
+const 
