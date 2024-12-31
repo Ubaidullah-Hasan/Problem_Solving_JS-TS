0025c5eab85c5069347c8b2e46c85ed06125167c
Ubaidullah <ubaidullhasan2023@gmail.com>
2024-12-31 12:25:21 +0600

    array_mastering.js file

---
+++ array_mastering.js
@@ -0,0 +1,12 @@
+//  T-001: Create an array of 5 elements using the Array Constructor.
+const newArr = new Array(5, 1);
+
+// T-002: Create an array of 3 empty slots.
+const epltyArr = new Array(3);
+
+// T-003: Create an array of 6 elements using the Array literals and access the fourth element in the array using its length property.
+const sixElementsOfArr = ["one", "two", "three", "four", "five", "six"];
+const fourthEle = sixElementsOfArr[3];
+console.log(fourthEle);
+
+// T-004: Use the for loop on the above array to print elements in the odd index.
